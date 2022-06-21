0b0b4ace82fb0b790178a99ffe1185472eb08407
Henrik <henrik.ruan4@gmail.com>
2022-06-21 20:58:08 -0300

    exercícios do dia 1 do bloco 4

---
+++ 1.Fundamentos/Bloco-04/Aula-4.1 Introdução à JavaScript e Lógica de Programação/Exercício/calculadora.js
@@ -0,0 +1,17 @@
+// Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const a = 1;
+const b = 1;
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a % b);
---
+++ 1.Fundamentos/Bloco-04/Aula-4.1 Introdução à JavaScript e Lógica de Programação/Exercício/converte nota em letras.js
@@ -0,0 +1,24 @@
+// Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// Porcentagem >= 90 -> A
+// Porcentagem >= 80 -> B
+// Porcentagem >= 70 -> C
+// Porcentagem >= 60 -> D
+// Porcentagem >= 50 -> E
+// Porcentagem < 50 -> F
+
+nota = 100;
+if (nota > 100 || nota < 0) {
+  console.log("[ERRO]");
+} else if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else if (nota < 50) {
+  console.log("F");
+}
