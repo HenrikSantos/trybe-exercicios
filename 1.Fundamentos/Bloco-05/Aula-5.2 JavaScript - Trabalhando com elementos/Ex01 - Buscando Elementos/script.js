08c46d36a97a5a3712c2e659e56bfe468e19e1d8
Henrik <henrik.ruan4@gmail.com>
2022-06-29 22:39:31 -0300

    exercicios do conteudo bloco 5 dia 2

--- 1.Fundamentos/Bloco-05/Aula-5.2 JavaScript - Trabalhando com elementos/Ex01 - Buscando Elementos/script.js
+++ 1.Fundamentos/Bloco-05/Aula-5.2 JavaScript - Trabalhando com elementos/Ex01 - Buscando Elementos/script.js
@@ -1,11 +1,33 @@
-// arquivo script.js
+// 1. Acesse o elemento elementoOndeVoceEsta.
+const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+const pai = elementoOndeVoceEsta.parentElement;
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+primeiroFilhoDoFilho.innerText = "Olá";
+// 4. Acesse o primeiroFilho a partir de pai.
+const primeiroFilho = pai.firstChild;
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+const primeiroFilhoAPartirDoElementoOndeVoceEsta =
+  elementoOndeVoceEsta.previousElementSibling;
+// 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+const atencao = primeiroFilho.nextSibling;
+console.log(atencao);
+// 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+const terceiroFilho = primeiroFilho.nextElementSibling.nextElementSibling;
+// 8. Agora acesse o terceiroFilho a partir de pai.
+const terceiroFilhoAPartirDoPai = pai.lastChild;
 
-const primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho')
-const pai = document.querySelector('#pai')
-
-// 1. Acesse o elemento elementoOndeVoceEsta. 
-const elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta')
-elementoOndeVoceEsta.parentElement.style.color = 'red'
-
-primeiroFilhoDoFilho.innerText = 'Olá este é o primeiroFilhoDoFilho'
-pai.firstChild
+// 1. Crie um irmão para elementoOndeVoceEsta.
+const irmaoDoOndeVoceEsta = document.createElement("section");
+irmaoDoOndeVoceEsta.innerText = "Teste irmao";
+pai.appendChild(irmaoDoOndeVoceEsta);
+// 2. Crie um filho para elementoOndeVoceEsta.
+const filhoDoOndeVoceEsta = document.createElement("section");
+elementoOndeVoceEsta.appendChild(filhoDoOndeVoceEsta);
+// 3. Crie um filho para primeiroFilhoDoFilho.
+const filhoprimeiroFilhoDoFilho = document.createElement("section");
+elementoOndeVoceEsta.appendChild(filhoprimeiroFilhoDoFilho);
+// 4. A partir desse filho criado, acesse terceiroFilho.
+terceiroFilhoAPartirDofilhoprimeiroFilhoDoFilho =
+  filhoprimeiroFilhoDoFilho.parentElement.nextElementSibling.nextElementSibling;
