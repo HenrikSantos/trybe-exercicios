e975f0a3eebcc41fa6da5aa785a909612f2e5711
Henrik <henrik.ruan4@gmail.com>
2022-07-21 19:28:19 -0300

    exercicios dia 1 bloco 8

---
+++ 1.Fundamentos/Bloco-08/Aula-8.1 JavaScript ES6 - Introdução a Higher Order Functions/Ex01 - Estruturando uma HOF/script.js
@@ -0,0 +1,11 @@
+// 1 - Crie uma função que retorne a string 'Acordando!!';
+// 2 - Crie outra função que retorne a string 'Bora tomar café!!';
+// 3 - Crie mais uma função que retorne a string 'Partiu dormir!!';
+// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores.
+const acordando = () => 'Acordando'
+const cafe = () => 'Bora tomar café!!'
+const dormir = () => 'Partiu dormir!!'
+
+const doingThings = (func) => func();
+
+console.log(doingThings(dormir))
